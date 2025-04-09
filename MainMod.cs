13b31a45e3ad8720f80bca5674713d52a034b536
SamP10 <sam.plant998@gmail.com>
2025-04-09 17:06:17 +0100

    ADD initial commit

--- MainMod.cs
+++ MainMod.cs
@@ -1,7 +1,7 @@
 using MelonLoader;
 using HarmonyLib;
-using ScheduleOne.Product;
-using ScheduleOne.PlayerScripts;
+// using ScheduleOne.Product;
+// using ScheduleOne.PlayerScripts;
 
 [assembly: MelonInfo(typeof(ExampleMod.ExampleMod), ExampleMod.BuildInfo.Name, ExampleMod.BuildInfo.Version, ExampleMod.BuildInfo.Author, ExampleMod.BuildInfo.DownloadLink)]
 [assembly: MelonColor()]
@@ -11,24 +11,23 @@ namespace ExampleMod
 {
     public static class BuildInfo
     {
-        public const string Name = "Example";
-        public const string Description = "";
-        public const string Author = "XOWithSauce";
-        public const string Company = null;
+        public const string Name = "Better Throwing";
+        public const string Description = "A mod that improves throwing mechanics. Such as knockback on NPC's and a throw strength meter.";
+        public const string Author = "3Exchange";
         public const string Version = "1.0";
         public const string DownloadLink = null;
     }
 
     public class ExampleMod : MelonMod
     {
-        [HarmonyPatch(typeof(Player), "ConsumeProduct")]
+        // [HarmonyPatch(typeof(Player), "ConsumeProduct")]
         public static class Player_ConsumeProduct_Patch
         {
-            public static bool Prefix(Player __instance, ProductItemInstance product)
-            {
-                MelonLogger.Msg("Product is being consumed");
-                return true;
-            }
+            // public static bool Prefix(Player __instance, ProductItemInstance product)
+            // {
+            //     MelonLogger.Msg("Product is being consumed");
+            //     return true;
+            // }
         }
     }
 }
